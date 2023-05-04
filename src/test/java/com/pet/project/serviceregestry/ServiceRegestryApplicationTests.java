c4dcebb857250ad01afc5e190055fd6414cc7679
kiryl.kostyleu <kiryl.kostyleu@sap.com>
2023-05-04 17:16:14 +0300

    Add service-regestry

---
+++ src/test/java/com/pet/project/serviceregestry/ServiceRegestryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet.project.serviceregestry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegestryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
